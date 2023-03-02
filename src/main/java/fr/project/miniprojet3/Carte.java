1934a88d53c8759d0ce1b14df6e7fd3014a188a4
Tanou81 <55876186+Tanou81@users.noreply.github.com>
2023-03-02 20:38:46 +0100

    timer ok retournement des cartes ok point ok

--- src/main/java/fr/project/miniprojet3/Carte.java
+++ src/main/java/fr/project/miniprojet3/Carte.java
@@ -1,8 +1,37 @@
 package fr.project.miniprojet3;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Carte {
-    private int i ;
-    public Carte(){
-        this.i = 1 ;
+    private String  nom  ;
+    private String  url   ;
+    private Image myimage ;
+    private ImageView myimageVieux ;
+
+    public Carte(String nom , String url){
+        this.nom  = nom  ;
+        this.url = url;
+        this.myimage = new Image(url);
+        this.myimageVieux = new ImageView(this.myimage);
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public Image getMyimage(){
+        return myimage;
+    }
+
+
+    public ImageView getMyimageVieux(){
+        return  this.myimageVieux;
+    }
+
+
 }
--- src/main/java/fr/project/miniprojet3/Model.java
+++ src/main/java/fr/project/miniprojet3/Model.java
@@ -4,8 +4,38 @@ public class Model {
     private int score ;
     private int temps ;
 
+    private Carte roi ;
+    private Carte dame ;
+    private Carte dos  ;
+    private Carte valet  ;
+
     /*a les données
     donne objet metier
     de cartes et la logique pour vérifier si
 les cartes sont assorties ou non*/
+    public Model(){
+        this.roi = new Carte("roi", "C:\\Users\\natha\\IdeaProjects\\miniprojet3\\src\\main\\mesImages\\roi.PNG");
+        this.dame = new Carte("dame", "C:\\Users\\natha\\IdeaProjects\\miniprojet3\\src\\main\\mesImages\\dame.PNG");
+        this.valet = new Carte("dame", "C:\\Users\\natha\\IdeaProjects\\miniprojet3\\src\\main\\mesImages\\valet.PNG");
+        this.dos = new Carte("dos", "C:\\Users\\natha\\IdeaProjects\\miniprojet3\\src\\main\\mesImages\\dos.JPEG");
+    }
+
+    public Carte getDame() {
+        return dame;
+    }
+
+    public Carte getDos() {
+        return dos;
+    }
+
+    public Carte getValet() {
+        return valet;
+    }
+
+    public Carte getRoi() {
+        return roi;
+    }
+
+
+
 }
